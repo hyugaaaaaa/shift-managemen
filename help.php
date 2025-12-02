163a50183019b35cb496e27fcb2a8fbb8c237c47
hyugaaaaaa <hyuga0510@icloud.com>
2025-12-02 22:32:13 +0900

    ヘルプ機能の追加

---
+++ help.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/config.php';
+session_start();
+require_once __DIR__ . '/template.php';
+
+// ログイン確認
+if (empty($_SESSION['user_id'])) {
+    header('Location: ' . BASE_PATH . '/index.php');
+    exit;
+}
+
+// ビューの読み込み
+require_once __DIR__ . '/views/help_view.php';
